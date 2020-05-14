e77268af27296f590b025701337005a68f63ca5b
unknown <453709680@qq.com>
2020-05-14 22:44:51 +0800

    '获取服务器数据'

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>    
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBar'
+}
+</script>
+
+<style scoped>
+  #tab-bar{
+    display:flex;
+    background:#f6f6f6;
+    height:49px;
+    position:fixed;
+    left:0;
+    right:0;
+    bottom:0;
+    box-shadow:0 -1px 1px rgba(255,192,203,.2);
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.export = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        // 'common':'@/common',
+        'components':'@/components',
+        // 'network':'@/network',
+        // 'views':'@/views',
+      }
+    }
+  }
+}
